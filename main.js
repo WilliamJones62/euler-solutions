7de55844806595b89bea221402f477cc8ee92b10
williamjones62 <williamjones62@gmail.com>
2017-05-16 16:48:35 -0400

    Add code to demonstrate closures in a JS application

--- main.js
+++ main.js
@@ -1,85 +1,9 @@
-main();
-
 function main() {
-  var yesterdaysOrders = [
-        {
-            id: 1,
-            orderLines: [
-                { itemName: "Item 01", quantity: 1 },
-                { itemName: "Item 02", quantity: 3 },
-                { itemName: "Item 03", quantity: 25 },
-                { itemName: "Item 04", quantity: 12 },
-            ],
-        },
-        {
-            id: 2,
-            orderLines: [
-                { itemName: "Item 01", quantity: 1 },
-                { itemName: "Item 08", quantity: 42 },
-                { itemName: "Item 09", quantity: 13 },
-                { itemName: "Item 12", quantity: 37 },
-            ],
-        },
-        {
-            id: 3,
-            orderLines: [
-                { itemName: "Item 12", quantity: 16 },
-            ],
-        },
-        {
-            id: 4,
-            orderLines: [
-                { itemName: "Item 10", quantity: 11 },
-                { itemName: "Item 11", quantity: 10 },
-            ],
-        },
-        {
-            id: 5,
-            orderLines: [
-                { itemName: "Item 06", quantity: 7 },
-                { itemName: "Item 07", quantity: 2 },
-                { itemName: "Item 12", quantity: 14 },
-            ],
-        },
-        {
-            id: 6,
-            orderLines: [
-                { itemName: "Item 05", quantity: 17 },
-            ],
-        },
-        {
-            id: 7,
-            orderLines: [
-                { itemName: "Item 03", quantity: 5 },
-                { itemName: "Item 07", quantity: 2 },
-            ],
-        },
-        {
-            id: 8,
-            orderLines: [
-                { itemName: "Item 02", quantity: 13 },
-                { itemName: "Item 07", quantity: 7 },
-                { itemName: "Item 09", quantity: 2 },
-            ],
-        },
-        {
-            id: 9,
-            orderLines: [
-                { itemName: "Item 01", quantity: 4 },
-                { itemName: "Item 06", quantity: 17 },
-                { itemName: "Item 07", quantity: 3 },
-            ],
-        },
-        {
-            id: 10,
-            orderLines: [
-                { itemName: "Item 11", quantity: 12 },
-                { itemName: "Item 12", quantity: 1 },
-            ],
-        },
-    ];
+  var orderRepo = new OrderRepository ();
+  var yesterday = orderRepo.getYesterdaysOrders();
+
   // create an array containing the itemName and the total quantity of that item picked yesterday
-  var yesterdaysTotals = combineQuantities(yesterdaysOrders;
+  var yesterdaysTotals = combineQuantities(yesterday);
   var shelfItems = sortTotals(yesterdaysTotals);
 
   for (i = 0; i < shelfItems.length; i++) {
@@ -158,6 +82,8 @@ function sortTotals(totals) {
 }
 
 function displayShelfItemPair(shelfName, itemName) {
-  console.log(shelfName);
-  console.log(itemName);
+  var newP = document.createElement('p');
+  newP.innerHTML = shelfName + "  " + itemName;
+
+  document.body.appendChild(newP);
 }
